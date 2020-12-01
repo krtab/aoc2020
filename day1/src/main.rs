9e10a95bd23f12ea61b63e4c6a2b4ed22da1b919
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-12-01 10:32:34 +0100

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,37 @@
+use std::cmp::{Ord,Ordering};
+use std::fs;
+use std::str::{self,FromStr};
+use anyhow;
+
+fn main() -> anyhow::Result<()> {
+    let buf = fs::read("input.txt")?;
+    let s = str::from_utf8(&buf)?;
+    let input : Result<Vec<i64>,<i64 as FromStr>::Err> = s.lines().map(|x| (x.parse())).collect();
+    let mut input = input?;
+    input.sort_unstable();
+    let mut iter = input.iter();
+    let mut left = iter.next();
+    let mut right = iter.next_back();
+    while let Some((l,r)) = left.and_then(|l| right.map(|r| (l,r))) {
+        use Ordering::*;
+        let ord = Ord::cmp( &(l+r), &2020);
+        match ord {
+            Less => left = iter.next(),
+            Greater => right = iter.next_back(),
+            Equal => {
+                println!("Result: {}x{} = {}", l, r, l*r);
+                break
+            }
+        }
+    }
+    for i1 in &input {
+        for i2 in &input {
+            for i3 in &input {
+                if i1 + i2 + i3 == 2020 {
+                    println!("Result: {} x {} x {} = {}", i1, i2, i3, i1*i2*i3);
+                }
+            }
+        }
+    }
+    Ok(())
+}
