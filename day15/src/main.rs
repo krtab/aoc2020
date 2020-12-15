7839e75adc720a1b530e1f13b8a6e4bc2b69bfa6
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-12-15 13:46:15 +0100

    Perf ugliness

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,26 +1,27 @@
-use std::{
-    str::FromStr,
-};
-use vec_map::VecMap as HashMap;
+use std::{num::NonZeroU32 as T, str::FromStr};
 
 fn main() -> anyhow::Result<()> {
-    let buf = String::from_utf8(std::fs::read("input.txt")?)?;
-    let input: Result<Vec<_>, _> = buf.split(',').map(usize::from_str).collect();
-    let input = input?;
-    let mut history = HashMap::with_capacity(30_000_000);
-    let mut turn = 0;
+    // let buf = String::from_utf8(std::fs::read("input.txt")?)?;
+    // let input: Result<Vec<_>, _> = buf.split(',').map(usize::from_str).collect();
+    let input = vec![6, 3, 15, 13, 1, 0];
+    let mut history = vec![None; 30_000_000];
+    let mut turn = 1;
     for i in input {
-        history.insert(i, turn);
+        unsafe {
+            *history.get_unchecked_mut(i) = Some(T::new(turn).unwrap());
+        };
         turn += 1;
     }
     let mut next_to_say = 0;
-    while turn < 30000000-1 {
+    while turn < 30000000 {
         // eprintln!("{} : {}", turn, next_to_say);
-        let next = history
-            .insert(next_to_say, turn)
-            .map(|prev_turn| turn - prev_turn)
-            .unwrap_or(0);
-        next_to_say = next;
+        let tmp = unsafe { history.get_unchecked_mut(next_to_say) };
+        let old = std::mem::replace(tmp, unsafe { Some(T::new_unchecked(turn)) });
+        let next = old.map(|prev_turn| unsafe { T::new_unchecked(turn - prev_turn.get()) });
+        next_to_say = match next {
+            None => 0,
+            Some(x) => x.get() as usize,
+        };
         turn += 1;
     }
     // dbg!(history.keys().max().unwrap());
