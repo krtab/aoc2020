ac17b84da0d20bac8cafc1efdd520b719bf6a4cd
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-12-16 00:09:00 +0100

    Perf but more elegant

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,30 +1,25 @@
-use std::{num::NonZeroU32 as T, str::FromStr};
+type T = u32;
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     // let buf = String::from_utf8(std::fs::read("input.txt")?)?;
     // let input: Result<Vec<_>, _> = buf.split(',').map(usize::from_str).collect();
+
     let input = vec![6, 3, 15, 13, 1, 0];
-    let mut history = vec![None; 30_000_000];
+    let mut history = vec![T::MAX; 30_000_000];
     let mut turn = 1;
     for i in input {
         unsafe {
-            *history.get_unchecked_mut(i) = Some(T::new(turn).unwrap());
+            *history.get_unchecked_mut(i) = turn;
         };
         turn += 1;
     }
     let mut next_to_say = 0;
-    while turn < 30000000 {
-        // eprintln!("{} : {}", turn, next_to_say);
+    while turn < 30_000_000 {
         let tmp = unsafe { history.get_unchecked_mut(next_to_say) };
-        let old = std::mem::replace(tmp, unsafe { Some(T::new_unchecked(turn)) });
-        let next = old.map(|prev_turn| unsafe { T::new_unchecked(turn - prev_turn.get()) });
-        next_to_say = match next {
-            None => 0,
-            Some(x) => x.get() as usize,
-        };
+        let old = std::mem::replace(tmp, turn);
+        next_to_say = turn.saturating_sub(old) as usize;
         turn += 1;
     }
     // dbg!(history.keys().max().unwrap());
     println!("Answer1: {}", next_to_say);
-    Ok(())
 }
